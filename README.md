a6611b5a4619522c4d4079446da6a785e6e2ddb9
Franck Hild <franckh@MBP-Franckh.local>
2016-07-05 11:34:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Not my work. Put here with the permission of the creator
+See [here][https://www.npmjs.com/package/weso] for the original work
+## weso
+
+Universal websocket handler for both client and servers in JS
+
+for more details on the protocol, 
+
+
+    `npm install weso`
+
+
+### Usage example :
+
+checkout my [server](https://www.npmjs.com/package/weso-node) for node and [client](https://www.npmjs.com/package/weso-browser) for the browser
+
+
+## the default `protocol` :
+
+the `separator` is a colon (`':'`)
+
+the websocket message MUST start with a key that doesn't contains the `separator`
+
+    It is recommanded to use camelCase and only alphanumeric characters so the shorthands are handy to use later on in JS, but you are free to do what ever you want.
+
+The part after the `separator` is then parsed in JSON
+
+That's pretty much it.
---
+++ index.js
@@ -0,0 +1,103 @@
+'use strict';
+
+var Ev = require('geval/event');
+
+var defaultFormatContent = function defaultFormatContent(content) {
+  return JSON.stringify(content);
+};
+
+var defaultParser = function defaultParser(data) {
+  if (!data) return {};
+
+  var pos = data.indexOf(':');
+  var route = data.slice(0, pos);
+
+  return { route: route, data: JSON.parse(data.slice(pos + 1)) };
+};
+
+var checkRoute = function checkRoute(weso, route) {
+  if (weso[route]) throw new Error('Route already reserved ' + route);
+  if (/:/.test(route)) throw new Error('Invalid route ' + route);
+};
+
+module.exports = function (opts) {
+  var broadcasters = {};
+  var weso = Ev();
+
+  var formatContent = opts.formatContent || defaultFormatContent;
+  var parser = opts.parser || defaultParser;
+  var sub = opts.sub || opts.subscribe || [];
+  var pub = opts.pub || opts.publish || [];
+
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = sub[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var route = _step.value;
+
+      checkRoute(weso, route);
+      var ev = Ev();
+      broadcasters[route] = ev.broadcast;
+      weso[route] = ev.listen;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    var _loop = function _loop() {
+      var route = _step2.value;
+
+      checkRoute(weso, route);
+      var prefixedRoute = route + ':';
+      weso[route] = function (d) {
+        return weso.broadcast(prefixedRoute + formatContent(d));
+      };
+    };
+
+    for (var _iterator2 = pub[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      _loop();
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2.return) {
+        _iterator2.return();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
+  }
+
+  weso.onmessage = function (data, ws) {
+    var parsed = parser(data);
+    var broadcast = broadcasters[parsed.route];
+    if (!broadcast) return;
+
+    parsed.ws = ws;
+    broadcast(parsed);
+  };
+
+  return weso;
+};
